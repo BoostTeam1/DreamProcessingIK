bd1f064a530332648b7a0f11b77c90de46cacd92
hseyinsams <huseyinsamsun81@gmail.com>
2022-05-24 11:50:27 +0300

    add api

---
+++ DreamIK.API/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace DreamIK.API.Controllers
+{
+    [Route(template:"api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private UserManager<AppUser> _userManager;
+        public SignInManager<AppUser> _signInManager;
+        public RoleManager<AppRole> _roleManager;
+        public UserController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<AppRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+        [Route(template:"api/alluser")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var result = _userManager.Users.ToList();
+
+            return Ok(result);
+        }
+
+
+    }
+}
